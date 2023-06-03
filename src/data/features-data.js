a986298c60444dd7d2743ec25f3ae0e7a1d43b33
Aparna garg <aparnagargstg04@gmail.com>
2023-06-03 17:00:28 +0530

    adding entire website code on git

---
+++ src/data/features-data.js
@@ -0,0 +1,32 @@
+import {
+  StarIcon,
+  ArrowPathIcon,
+  FingerPrintIcon,
+} from "@heroicons/react/24/solid";
+
+
+export const featuresData = [
+  {
+    color: "blue",
+    title: "Rahul Johari",
+    icon: StarIcon,
+    description:
+      "mentor description",
+  },
+  {
+    color: "red",
+    title: "Free Revisions",
+    icon: ArrowPathIcon,
+    description:
+      "Keep you user engaged by providing meaningful information. Remember that by this time, the user is curious.",
+  },
+  {
+    color: "teal",
+    title: "Verified Company",
+    icon: FingerPrintIcon,
+    description:
+      "Write a few lines about each one. A paragraph describing a feature will be enough. Keep you user engaged!",
+  },
+];
+
+export default featuresData;
