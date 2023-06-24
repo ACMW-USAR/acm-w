9ca286e3a922c8c23e835cc9c88bd2f95c4bc624
Shubhangi Mishra <shubhangimishra3302@gmail.com>
2023-06-24 22:58:09 +0530

    clubs def

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -63,19 +63,12 @@ export function Home() {
                 className="mb-3 font-bold"
                 color="blue-gray"
               >
-                Working with us is a pleasure
+                DevSource
               </Typography>
               <Typography className="mb-8 font-normal text-blue-gray-500">
-                Don't let your uses guess by attaching tooltips and popoves to
-                any element. Just make sure you enable them first via
-                JavaScript.
-                <br />
-                <br />
-                The kit comes with three pre-built pages to help you get started
-                faster. You can change the text and images and you're good to
-                go. Just make sure you enable them first via JavaScript.
+              DevSource - The Development Group is a community of students highly enthusiastic about OpenSource, app, and Web Development. The club provides an opportunity for students with a common interest in the development and open source, to gather and develop together. DevSource organizes various events and workshops for the students to learn new technologies and build hands-on projects.
               </Typography>
-              <Button variant="outlined">read more</Button>
+              
             </div>
             <div className="mx-auto mt-24 flex w-full justify-center px-4 md:w-4/12 lg:mt-0">
               <Card className="shadow-lg shadow-gray-500/10">
@@ -97,11 +90,12 @@ export function Home() {
                   <Typography className="font-normal text-blue-gray-500">
                     The Arctic Ocean freezes every winter and much of the
                     sea-ice then thaws every summer, and that process will
-                    continue whatever happens.
+                    continue hi 12345678.
                   </Typography>
                 </CardBody>
               </Card>
             </div>
+                {/* end of the row */}
 
             <div className="mx-auto mt-24 flex w-full justify-center px-4 md:w-4/12 lg:mt-0">
               <Card className="shadow-lg shadow-gray-500/10">
@@ -149,12 +143,347 @@ export function Home() {
                 faster. You can change the text and images and you're good to
                 go. Just make sure you enable them first via JavaScript.
               </Typography>
-              <Button variant="outlined">read more</Button>
+              
+              
+              
             </div>
 
+                {/* //end of the row */}
+
+            <div className="mx-auto -mt-8 w-full px-4 md:w-5/12">
+              <div className="mb-6 inline-flex h-16 w-16 items-center justify-center rounded-full bg-white p-3 text-center shadow-lg">
+                <UsersIcon className="h-6 w-6 text-blue-gray-900" />
+              </div>
+              <Typography
+                variant="h3"
+                className="mb-3 font-bold"
+                color="blue-gray"
+              >
+                DevSource
+              </Typography>
+              <Typography className="mb-8 font-normal text-blue-gray-500">
+              DevSource - The Development Group is a community of students highly enthusiastic about OpenSource, app, and Web Development. The club provides an opportunity for students with a common interest in the development and open source, to gather and develop together. DevSource organizes various events and workshops for the students to learn new technologies and build hands-on projects.
+              </Typography>
             
+            </div>
+            <div className="mx-auto mt-24 flex w-full justify-center px-4 md:w-4/12 lg:mt-0">
+              <Card className="shadow-lg shadow-gray-500/10">
+                <CardHeader className="relative h-56">
+                  <img
+                    alt="Card Image"
+                    src="/img/teamwork.jpeg"
+                    className="h-full w-full"
+                  />
+                </CardHeader>
+                <CardBody>
+                  <Typography
+                    variant="h5"
+                    color="blue-gray"
+                    className="mb-3 font-bold"
+                  >
+                    Top Notch Services
+                  </Typography>
+                  <Typography className="font-normal text-blue-gray-500">
+                    The Arctic Ocean freezes every winter and much of the
+                    sea-ice then thaws every summer, and that process will
+                    continue hi 12345678.
+                  </Typography>
+                </CardBody>
+              </Card>
+            </div>
+
+          
+
+
+                {/* end of the row */}
+                <div className="mx-auto mt-24 flex w-full justify-center px-4 md:w-4/12 lg:mt-0">
+              <Card className="shadow-lg shadow-gray-500/10">
+                <CardHeader className="relative h-56">
+                  <img
+                    alt="Card Image"
+                    src="/img/teamwork.jpeg"
+                    className="h-full w-full"
+                  />
+                </CardHeader>
+                <CardBody>
+                  <Typography
+                    variant="h5"
+                    color="blue-gray"
+                    className="mb-3 font-bold"
+                  >
+                    Top Notch Services
+                  </Typography>
+                  <Typography className="font-normal text-blue-gray-500">
+                    The Arctic Ocean freezes every winter and much of the
+                    sea-ice then thaws every summer, and that process will
+                    continue whatever happens.
+                  </Typography>
+                </CardBody>
+              </Card>
+            </div>
+            <div className="mx-auto -mt-8 w-full px-4 md:w-5/12">
+              <div className="mb-6 inline-flex h-16 w-16 items-center justify-center rounded-full bg-white p-3 text-center shadow-lg">
+                <UsersIcon className="h-6 w-6 text-blue-gray-900" />
+              </div>
+              <Typography
+                variant="h3"
+                className="mb-3 font-bold"
+                color="blue-gray"
+              >
+                Working with us is a pleasure
+              </Typography>
+              <Typography className="mb-8 font-normal text-blue-gray-500">
+                Don't let your uses guess by attaching tooltips and popoves to
+                any element. Just make sure you enable them first via
+                JavaScript.
+                <br />
+                <br />
+                The kit comes with three pre-built pages to help you get started
+                faster. You can change the text and images and you're good to
+                go. Just make sure you enable them first via JavaScript.
+              </Typography>
+              
+              
+              
+            </div>
+            {/* end of the row */}
+
+                <div className="mx-auto -mt-8 w-full px-4 md:w-5/12">
+              <div className="mb-6 inline-flex h-16 w-16 items-center justify-center rounded-full bg-white p-3 text-center shadow-lg">
+                <UsersIcon className="h-6 w-6 text-blue-gray-900" />
+              </div>
+              <Typography
+                variant="h3"
+                className="mb-3 font-bold"
+                color="blue-gray"
+              >
+                DevSource
+              </Typography>
+              <Typography className="mb-8 font-normal text-blue-gray-500">
+              DevSource - The Development Group is a community of students highly enthusiastic about OpenSource, app, and Web Development. The club provides an opportunity for students with a common interest in the development and open source, to gather and develop together. DevSource organizes various events and workshops for the students to learn new technologies and build hands-on projects.
+              </Typography>
+             
+            </div>
+            <div className="mx-auto mt-24 flex w-full justify-center px-4 md:w-4/12 lg:mt-0">
+              <Card className="shadow-lg shadow-gray-500/10">
+                <CardHeader className="relative h-56">
+                  <img
+                    alt="Card Image"
+                    src="/img/teamwork.jpeg"
+                    className="h-full w-full"
+                  />
+                </CardHeader>
+                <CardBody>
+                  <Typography
+                    variant="h5"
+                    color="blue-gray"
+                    className="mb-3 font-bold"
+                  >
+                    Top Notch Services
+                  </Typography>
+                  <Typography className="font-normal text-blue-gray-500">
+                    The Arctic Ocean freezes every winter and much of the
+                    sea-ice then thaws every summer, and that process will
+                    continue hi 12345678.
+                  </Typography>
+                </CardBody>
+              </Card>
+            </div>
+
+                {/* end of the row */}
+
+                <div className="mx-auto mt-24 flex w-full justify-center px-4 md:w-4/12 lg:mt-0">
+              <Card className="shadow-lg shadow-gray-500/10">
+                <CardHeader className="relative h-56">
+                  <img
+                    alt="Card Image"
+                    src="/img/teamwork.jpeg"
+                    className="h-full w-full"
+                  />
+                </CardHeader>
+                <CardBody>
+                  <Typography
+                    variant="h5"
+                    color="blue-gray"
+                    className="mb-3 font-bold"
+                  >
+                    Top Notch Services
+                  </Typography>
+                  <Typography className="font-normal text-blue-gray-500">
+                    The Arctic Ocean freezes every winter and much of the
+                    sea-ice then thaws every summer, and that process will
+                    continue whatever happens.
+                  </Typography>
+                </CardBody>
+              </Card>
+            </div>
+            <div className="mx-auto -mt-8 w-full px-4 md:w-5/12">
+              <div className="mb-6 inline-flex h-16 w-16 items-center justify-center rounded-full bg-white p-3 text-center shadow-lg">
+                <UsersIcon className="h-6 w-6 text-blue-gray-900" />
+              </div>
+              <Typography
+                variant="h3"
+                className="mb-3 font-bold"
+                color="blue-gray"
+              >
+                Working with us is a pleasure
+              </Typography>
+              <Typography className="mb-8 font-normal text-blue-gray-500">
+                Don't let your uses guess by attaching tooltips and popoves to
+                any element. Just make sure you enable them first via
+                JavaScript.
+                <br />
+                <br />
+                The kit comes with three pre-built pages to help you get started
+                faster. You can change the text and images and you're good to
+                go. Just make sure you enable them first via JavaScript.
+              </Typography>
+             
+              
+              
+            </div>
+            {/* end of the row */}
+            <div className="mx-auto -mt-8 w-full px-4 md:w-5/12">
+              <div className="mb-6 inline-flex h-16 w-16 items-center justify-center rounded-full bg-white p-3 text-center shadow-lg">
+                <UsersIcon className="h-6 w-6 text-blue-gray-900" />
+              </div>
+              <Typography
+                variant="h3"
+                className="mb-3 font-bold"
+                color="blue-gray"
+              >
+                DevSource
+              </Typography>
+              <Typography className="mb-8 font-normal text-blue-gray-500">
+              DevSource - The Development Group is a community of students highly enthusiastic about OpenSource, app, and Web Development. The club provides an opportunity for students with a common interest in the development and open source, to gather and develop together. DevSource organizes various events and workshops for the students to learn new technologies and build hands-on projects.
+              </Typography>
+              
+            </div>
+            <div className="mx-auto mt-24 flex w-full justify-center px-4 md:w-4/12 lg:mt-0">
+              <Card className="shadow-lg shadow-gray-500/10">
+                <CardHeader className="relative h-56">
+                  <img
+                    alt="Card Image"
+                    src="/img/teamwork.jpeg"
+                    className="h-full w-full"
+                  />
+                </CardHeader>
+                <CardBody>
+                  <Typography
+                    variant="h5"
+                    color="blue-gray"
+                    className="mb-3 font-bold"
+                  >
+                    Top Notch Services
+                  </Typography>
+                  <Typography className="font-normal text-blue-gray-500">
+                    The Arctic Ocean freezes every winter and much of the
+                    sea-ice then thaws every summer, and that process will
+                    continue hi 12345678.
+                  </Typography>
+                </CardBody>
+              </Card>
+            </div>
+
+                {/* end of the row */}
+                <div className="mx-auto mt-24 flex w-full justify-center px-4 md:w-4/12 lg:mt-0">
+              <Card className="shadow-lg shadow-gray-500/10">
+                <CardHeader className="relative h-56">
+                  <img
+                    alt="Card Image"
+                    src="/img/teamwork.jpeg"
+                    className="h-full w-full"
+                  />
+                </CardHeader>
+                <CardBody>
+                  <Typography
+                    variant="h5"
+                    color="blue-gray"
+                    className="mb-3 font-bold"
+                  >
+                    Top Notch Services
+                  </Typography>
+                  <Typography className="font-normal text-blue-gray-500">
+                    The Arctic Ocean freezes every winter and much of the
+                    sea-ice then thaws every summer, and that process will
+                    continue whatever happens.
+                  </Typography>
+                </CardBody>
+              </Card>
+            </div>
+            <div className="mx-auto -mt-8 w-full px-4 md:w-5/12">
+              <div className="mb-6 inline-flex h-16 w-16 items-center justify-center rounded-full bg-white p-3 text-center shadow-lg">
+                <UsersIcon className="h-6 w-6 text-blue-gray-900" />
+              </div>
+              <Typography
+                variant="h3"
+                className="mb-3 font-bold"
+                color="blue-gray"
+              >
+                Working with us is a pleasure
+              </Typography>
+              <Typography className="mb-8 font-normal text-blue-gray-500">
+                Don't let your uses guess by attaching tooltips and popoves to
+                any element. Just make sure you enable them first via
+                JavaScript.
+                <br />
+                <br />
+                The kit comes with three pre-built pages to help you get started
+                faster. You can change the text and images and you're good to
+                go. Just make sure you enable them first via JavaScript.
+              </Typography>
+              
+              
+              
+            </div>
+            {/* end of the row */}
+            <div className="mx-auto -mt-8 w-full px-4 md:w-5/12">
+              <div className="mb-6 inline-flex h-16 w-16 items-center justify-center rounded-full bg-white p-3 text-center shadow-lg">
+                <UsersIcon className="h-6 w-6 text-blue-gray-900" />
+              </div>
+              <Typography
+                variant="h3"
+                className="mb-3 font-bold"
+                color="blue-gray"
+              >
+                DevSource
+              </Typography>
+              <Typography className="mb-8 font-normal text-blue-gray-500">
+              DevSource - The Development Group is a community of students highly enthusiastic about OpenSource, app, and Web Development. The club provides an opportunity for students with a common interest in the development and open source, to gather and develop together. DevSource organizes various events and workshops for the students to learn new technologies and build hands-on projects.
+              </Typography>
+              
+            </div>
+            <div className="mx-auto mt-24 flex w-full justify-center px-4 md:w-4/12 lg:mt-0">
+              <Card className="shadow-lg shadow-gray-500/10">
+                <CardHeader className="relative h-56">
+                  <img
+                    alt="Card Image"
+                    src="/img/teamwork.jpeg"
+                    className="h-full w-full"
+                  />
+                </CardHeader>
+                <CardBody>
+                  <Typography
+                    variant="h5"
+                    color="blue-gray"
+                    className="mb-3 font-bold"
+                  >
+                    Top Notch Services
+                  </Typography>
+                  <Typography className="font-normal text-blue-gray-500">
+                    The Arctic Ocean freezes every winter and much of the
+                    sea-ice then thaws every summer, and that process will
+                    continue hi 12345678.
+                  </Typography>
+                </CardBody>
+              </Card>
+            </div>
+
+                {/* end of the row */}
           </div>
+         
         </div>
+        
       </section>
       <section className="px-4 pt-20 pb-48">
         <div className="container mx-auto">
