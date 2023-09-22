4b952dd22fdab1e12457f1d157f87afe0d4c2d55
Aparna garg <aparnagargstg04@gmail.com>
2023-09-22 12:07:34 +0530

    ..

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
+    alias: [
+      { find: "@", replacement: "/src" },
+      // Add an alias for the @heroicons/react package
+      {
+        find: /^@heroicons\/react\/(.*)$/,
+        replacement: "@heroicons/react/$1",
+      },
+    ],
   },
 });
