a64b23de3cc535d72d252a3a8d06af780971e776
Gauranshi Gupta <95520163+ggauranshi-03@users.noreply.github.com>
2023-12-24 23:58:24 +0530

    image path corrected

--- src/data/team-data.js
+++ src/data/team-data.js
@@ -1,6 +1,4 @@
 export const teamData = [
-
-  
   {
     img: "/img/SAN_6748.JPG",
     name: "Gauranshi Gupta",
@@ -9,25 +7,25 @@ export const teamData = [
       {
         color: "light-blue",
         name: "twitter",
-        link:"https://twitter.com/Gupta7Gauranshi",
+        link: "https://twitter.com/Gupta7Gauranshi",
       },
       {
         color: "blue",
         name: "linkedin",
-        link:" https://www.linkedin.com/in/gauranshi-gupta-414865227/" ,
+        link: " https://www.linkedin.com/in/gauranshi-gupta-414865227/",
       },
       {
         color: "pink",
         name: "github",
-        link:"https://github.com/ggauranshi-03",
+        link: "https://github.com/ggauranshi-03",
       },
     ],
   },
- // {
-    //img: "public/img/aparna4.jpeg",
-    //name: "Aparna Garg",
-    //position: "Vice Chair",
-    //socials: [
+  // {
+  //img: "public/img/aparna4.jpeg",
+  //name: "Aparna Garg",
+  //position: "Vice Chair",
+  //socials: [
   //     {
   //       color: "light-blue",
   //       name: "twitter",
@@ -45,232 +43,227 @@ export const teamData = [
   //     },
   //   ],
   // },
-  
+
   {
-    img: "public/img/ayushi3.jpg",
+    img: "/img/ayushi3.jpg",
     name: "Ayushi Gupta",
     position: " Secretary",
     socials: [
       {
         color: "light-blue",
         name: "twitter",
-        link:"",
+        link: "",
       },
       {
         color: "blue",
         name: "linkedin",
-        link:"https://www.linkedin.com/in/ayushi-gupta-a96323227",
+        link: "https://www.linkedin.com/in/ayushi-gupta-a96323227",
       },
       {
         color: "pink",
         name: "github",
-        link:"https://github.com/Ayushi0405",
+        link: "https://github.com/Ayushi0405",
       },
     ],
   },
   {
-    img: "public/img/khsuhbu2.jpg",
+    img: "/img/khsuhbu2.jpg",
     name: "Khusbu Rai",
     position: "Joint Secretary",
     socials: [
       {
         color: "light-blue",
         name: "twitter",
-        link:"https://twitter.com/Khusburai03?t=QIQN5nnVHS5TB1CG32-vJQ&s=08",
+        link: "https://twitter.com/Khusburai03?t=QIQN5nnVHS5TB1CG32-vJQ&s=08",
       },
       {
         color: "blue",
         name: "linkedin",
-        link:"https://www.linkedin.com/in/khusbu-rai-37bb5a256",
+        link: "https://www.linkedin.com/in/khusbu-rai-37bb5a256",
       },
       {
         color: "pink",
         name: "github",
-        link:"https://github.com/khusburai28",
+        link: "https://github.com/khusburai28",
       },
     ],
   },
-   {
-     img: "public/img/aparna4.jpeg",
+  {
+    img: "/img/aparna4.jpeg",
     name: "Aparna Garg",
     position: "Web Chair",
     socials: [
       {
         color: "light-blue",
         name: "twitter",
-          link:"https://twitter.com/AparnaGarg18?t=Hzj5414eBqGbC4GNk8pcLQ&s=09",
+        link: "https://twitter.com/AparnaGarg18?t=Hzj5414eBqGbC4GNk8pcLQ&s=09",
       },
       {
         color: "blue",
         name: "linkedin",
-        link:"https://www.linkedin.com/in/aparna-garg-661267225/",
+        link: "https://www.linkedin.com/in/aparna-garg-661267225/",
       },
       {
-         color: "pink",
+        color: "pink",
         name: "github",
-        link:"https://github.com/aparnagarg04",
+        link: "https://github.com/aparnagarg04",
       },
     ],
-   },
-   {
-     img: "public/img/tina6.jpg",
-     name: "Tina Goel",
-     position: "Membership Chair",
-     socials: [
+  },
+  {
+    img: "/img/tina6.jpg",
+    name: "Tina Goel",
+    position: "Membership Chair",
+    socials: [
       {
         color: "light-blue",
         name: "twitter",
-        link:"https://twitter.com/Tinagoel884?t=-P_uMyC2rUHAy46O_a4uFw&s=09",
+        link: "https://twitter.com/Tinagoel884?t=-P_uMyC2rUHAy46O_a4uFw&s=09",
       },
       {
-       color: "blue",
-         name: "linkedin",
-         link:"https://www.linkedin.com/in/tina-goel-3652a121a",
+        color: "blue",
+        name: "linkedin",
+        link: "https://www.linkedin.com/in/tina-goel-3652a121a",
       },
       {
-         color: "pink",
-         name: "github",
-         link:"https://github.com/tina2703",
-       },
-     ],
-   },
-   {
-     img: "public/img/vanshika1.jpg",
+        color: "pink",
+        name: "github",
+        link: "https://github.com/tina2703",
+      },
+    ],
+  },
+  {
+    img: "/img/vanshika1.jpg",
     name: "Vanshika Gupta",
     position: "Treasurer",
     socials: [
-       {
-         color: "light-blue",
-        name: "twitter",
-        link:"https://twitter.com/vanshikagu58548?t=lHwe7TmomOtrd_PvuIjOYg&s=08",
-      },
-       {
-         color: "blue",
-        name: "linkedin",
-        link:"https://linkedin.com/in/vanshika-gupta-a05a74214",
-      },
-      {
-        color: "pink",
-         name: "github",
-         link:"https://github.com/vanshikagupta210904",
-       },
-     ],
-   },
-   {
-     img: "public/img/teja2.jpg",
-    name: "Tejasvi Arneja",
-     position: "Executive Member",
-     socials: [
       {
         color: "light-blue",
-         name: "twitter",
-           link:"https://twitter.com/Tejasvi2112?t=9ot0PQA7DSGo7NoceVc4EA&s=08",
-       },
-       {
-         color: "blue",
-        name: "linkedin",
-        link:"https://www.linkedin.com/in/tejasviarneja21",
-      },
-      {
-        color: "pink",
-         name: "github",
-         link:"https://github.com/Tejasvi-Arneja",
-       },
-     ],
-   },
-   {
-    img: "public/img/tanvi.jpg",
-    name: "Tanvi Gupta",
-     position: "Executive Member",
-     socials: [
-       {
-        color: "light-blue",
         name: "twitter",
-        link:"https://twitter.com/TanviGupta7623",
+        link: "https://twitter.com/vanshikagu58548?t=lHwe7TmomOtrd_PvuIjOYg&s=08",
       },
       {
         color: "blue",
         name: "linkedin",
-        link:"https://www.linkedin.com/in/tanvi-gupta-40025a1b9",
-       },
-       {
+        link: "https://linkedin.com/in/vanshika-gupta-a05a74214",
+      },
+      {
         color: "pink",
         name: "github",
-        link:"https://github.com/TanviGupta7623",
+        link: "https://github.com/vanshikagupta210904",
       },
-     ],
-   },
-   {
-     img: "public/img/shubhangi1.jpg",
-      name: "Shubhangi Mishra",
+    ],
+  },
+  {
+    img: "/img/teja2.jpg",
+    name: "Tejasvi Arneja",
     position: "Executive Member",
     socials: [
       {
         color: "light-blue",
         name: "twitter",
-        link:"https://twitter.com/Shubhangi_Billi",
+        link: "https://twitter.com/Tejasvi2112?t=9ot0PQA7DSGo7NoceVc4EA&s=08",
       },
       {
         color: "blue",
-         name: "linkedin",
-         link:"https://www.linkedin.com/in/shubhangimishra3302/",
-      },
-       {
-         color: "pink",
-         name: "github",
-         link:"https://github.com/shubhangimishra3",
-       },
-     ],
-   },
-   {
-   img: "public/img/sia1.jpg",
-     name: "Sia Gupta",
-    position: "Executive Member",
-    socials: [
-      {
-        color: "light-blue",
-        name: "twitter",
-        link:"https://twitter.com/siagupta_03",
-      },
-      {
-        color: "blue",
-         name: "linkedin",
-         link:"https://www.linkedin.com/in/sia-gupta-79b344153/ ",
-       },
-       {
-         color: "pink",
-         name: "github",
-         link:"  https://github.com/siagupta-03  ",
-       },
-     ],
-   },
-   {
-     img: "public/img/priyasvi.jpg",
-    name: "Priyasvi Sharma",
-     position: "Executive Member",
-    socials: [
-     {
-       color: "light-blue",
-        name: "twitter",
-        link:"https://twitter.com/priyasvi?t=sxScyiqWJIB1IlK7wTSNWQ&s=09",
-      },
-       {
-         color: "blue",
         name: "linkedin",
-        link:"https://www.linkedin.com/in/priyasvi-sharma-006664246",
+        link: "https://www.linkedin.com/in/tejasviarneja21",
       },
-       {
-         color: "pink",
-         name: "github",
-         link:"https://github.com/Priyasvi",
-       },
-     ],
-   },
-  
-  
-  
-  
-  
+      {
+        color: "pink",
+        name: "github",
+        link: "https://github.com/Tejasvi-Arneja",
+      },
+    ],
+  },
+  {
+    img: "/img/tanvi.jpg",
+    name: "Tanvi Gupta",
+    position: "Executive Member",
+    socials: [
+      {
+        color: "light-blue",
+        name: "twitter",
+        link: "https://twitter.com/TanviGupta7623",
+      },
+      {
+        color: "blue",
+        name: "linkedin",
+        link: "https://www.linkedin.com/in/tanvi-gupta-40025a1b9",
+      },
+      {
+        color: "pink",
+        name: "github",
+        link: "https://github.com/TanviGupta7623",
+      },
+    ],
+  },
+  {
+    img: "/img/shubhangi1.jpg",
+    name: "Shubhangi Mishra",
+    position: "Executive Member",
+    socials: [
+      {
+        color: "light-blue",
+        name: "twitter",
+        link: "https://twitter.com/Shubhangi_Billi",
+      },
+      {
+        color: "blue",
+        name: "linkedin",
+        link: "https://www.linkedin.com/in/shubhangimishra3302/",
+      },
+      {
+        color: "pink",
+        name: "github",
+        link: "https://github.com/shubhangimishra3",
+      },
+    ],
+  },
+  {
+    img: "/img/sia1.jpg",
+    name: "Sia Gupta",
+    position: "Executive Member",
+    socials: [
+      {
+        color: "light-blue",
+        name: "twitter",
+        link: "https://twitter.com/siagupta_03",
+      },
+      {
+        color: "blue",
+        name: "linkedin",
+        link: "https://www.linkedin.com/in/sia-gupta-79b344153/ ",
+      },
+      {
+        color: "pink",
+        name: "github",
+        link: "  https://github.com/siagupta-03  ",
+      },
+    ],
+  },
+  {
+    img: "/img/priyasvi.jpg",
+    name: "Priyasvi Sharma",
+    position: "Executive Member",
+    socials: [
+      {
+        color: "light-blue",
+        name: "twitter",
+        link: "https://twitter.com/priyasvi?t=sxScyiqWJIB1IlK7wTSNWQ&s=09",
+      },
+      {
+        color: "blue",
+        name: "linkedin",
+        link: "https://www.linkedin.com/in/priyasvi-sharma-006664246",
+      },
+      {
+        color: "pink",
+        name: "github",
+        link: "https://github.com/Priyasvi",
+      },
+    ],
+  },
 ];
 
 export default teamData;
