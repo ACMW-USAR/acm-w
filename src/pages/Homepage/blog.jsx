4beddc4083759c3f99baa6885b4eb245cfa8353c
Vanshika Gupta <vanshika.gupta210904@gmail.com>
2024-01-22 23:13:02 +0530

    merged blog code

---
+++ src/pages/Homepage/blog.jsx
@@ -0,0 +1,151 @@
+import React from "react";
+
+export const Blog = () => {
+    return (
+        <>
+        <div className="main_container">
+    
+    {/* <!--IF YOU WANT YOU CAN REMOVE THIS MAIN_CONTAINER JUST PASTE THE CARD SECTION AND IT WILL WORK FINE --> */}
+<h2>Blog Section</h2>
+    
+        <div className="card_container">
+    
+            
+                <div className="card_section">
+    
+    
+                    <div className="card">
+                        <a href="">
+                            <div className="card_image"><img src="images/stonks1.jpg" alt=""></img></div>
+                            <div className="card_panel">
+                                <div className="card_main_title"><h2>How to get started in stockmarket in 2022</h2></div>
+                                <div className="card_para"><p>a detailed post on how to invest in stocks</p></div>
+                                <div className="tags_section">
+                                    <ul>
+    
+                                        <li>#stocks</li>
+                                        <li>#shares</li>
+                                        <li>#NYSE</li>
+                                    </ul>
+                                </div>
+                                <div className="card_readmore"><span>Read More</span></div>
+                            </div>
+                        </a>
+                    </div>
+    
+    
+                    <div className="card">
+                        <a href="">
+                            <div className="card_image"><img src="images/photos1.jpg" alt=""></img></div>
+                            <div className="card_panel">
+                                <div className="card_main_title"><h2>Future Of Cyber-Security</h2></div>
+                                <div className="card_para"><p>learn how to protect yourselffrom data breaches stuff</p></div>
+                                <div className="tags_section">
+                                    <ul>
+    
+                                        <li>#security</li>
+                                        <li>#data</li>
+                                        <li>#resaerchers</li>
+                                    </ul>
+                                </div>
+                                <div className="card_readmore"><span>Read More</span></div>
+                            </div>
+                        </a>
+                    </div>
+    
+    
+    
+                    <div className="card">
+                        <a href="">
+                            <div className="card_image"><img src="images/photos3.jpg" alt=""></img></div>
+                            <div className="card_panel">
+                                <div className="card_main_title"><h2>AI Changing The World</h2></div>
+                                <div className="card_para"><p>learn about neural networks and linear algebra</p></div>
+                                <div className="tags_section">
+                                    <ul>
+    
+                                        <li>#AI</li>
+                                        <li>#Neural networks</li>
+                                        <li>#NLP</li>
+                                    </ul>
+                                </div>
+                                <div className="card_readmore"><span>Read More</span></div>
+                            </div>
+                        </a>
+                    </div>
+    
+    
+                    <div className="card">
+                        <a href="">
+                            <div className="card_image"><img src="images/photos4.jpg" alt=""></img></div>
+                            <div className="card_panel">
+                                <div className="card_main_title"><h2>Top 10 digital marketing strategies</h2></div>
+                                <div className="card_para"><p>a detailed post on social media  stuffs</p></div>
+                                <div className="tags_section">
+                                    <ul>
+    
+                                        <li>#SEO</li>
+                                        <li>#Offsite SEO</li>
+                                        <li>#Digital</li>
+                                    </ul>
+                                </div>
+                                <div className="card_readmore"><span>Read More</span></div>
+                            </div>
+                        </a>
+                    </div>
+    
+    
+                    <div className="card">
+                        <a href="">
+                            <div className="card_image"><img src="\img\stonks1.jpg" alt=""></img></div>
+                            <div className="card_panel">
+                                <div className="card_main_title"><h2>7 strategies to manage your time and work</h2></div>
+                               {/* <div className="card_para"><p>learn about maths in this blog posts</p></div> */}
+                                <div className="tags_section">
+                                    <ul>
+    
+                                        <li>#time</li>
+                                        <li>#work</li>
+                                        <li>#tips and tricks</li>
+                                    </ul>
+                                </div>
+                                <div className="card_readmore"><span>Read More</span></div>
+                            </div>
+                        </a>
+                    </div>
+    
+    
+                    <div className="card">
+                        <a href="">
+                            <div className="card_image"><img src="images/photos5.jpg" alt=""></img></div>
+                            <div className="card_panel">
+                                <div className="card_main_title"><h2>6 ways to manage your business</h2></div>
+                                <div className="card_para"><p>learn how you can  manage your business </p></div>
+                                <div className="tags_section">
+                                    <ul>
+    
+                                        <li>#strategies</li>
+                                        <li>#business</li>
+                                        <li>#manage</li>
+                                    </ul>
+                                </div>
+                                <div className="card_readmore"><span>Read More</span></div>
+                            </div>
+                        </a>
+                    </div>
+    
+    
+    
+    
+                </div>
+    
+    
+        </div>
+    
+    
+    </div>
+        </>
+    );
+
+};
+export default Blog;
