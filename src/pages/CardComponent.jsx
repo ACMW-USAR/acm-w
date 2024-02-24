cb8695b3adf1c400557bdda05216da117a7b0806
Prakriti0511 <prakriti0511@gmail.com>
2024-02-24 17:15:04 +0530

    changes in card components

--- src/pages/CardComponent.jsx
+++ src/pages/CardComponent.jsx
@@ -10,29 +10,11 @@ function CardComponent({ imageSrc, memberName, position }) {
   return (
     <div className="mx-auto mt-24 flex w-full justify-center px-4 md:w-4/12 lg:mt-0">
       <Card className="shadow-lg shadow-gray-500/10 transform hover:scale-105 transition-transform duration-300">
-        <CardHeader className="relative h-56">
-          <div className="flip-card" style={{ overflow: 'hidden' }}> {/* Add overflow: hidden here */}
-            <div className="flip-card-inner">
-              <div className="flip-card-front">
-                <img
+      <img
                   alt="Card Image"
                   src={imageSrc}
-                  className="h-full w-full"
-                />
-              </div>
-              <div className="flip-card-back flex justify-center items-center">
-                <Typography
-                  variant="h5"
-                  color="blue-gray"
-                  className="font-bold text-center"
-                  style={{ color: '#880e4f' }}
-                >
-                  {position}
-                </Typography>
-              </div>
-            </div>
-          </div>
-        </CardHeader>
+                  className="h-fullw-full"
+       />
         <CardBody>
           <Typography
             variant="h5"
